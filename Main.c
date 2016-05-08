6a941692750ea482e70b621ce020bd4ef1a98361
hideki16 <gabriel_shippo@hotmail.com>
2016-05-08 01:43:24 -0300

    tentando dar uma enxugada no codigo

--- Main.c
+++ Main.c
@@ -18,165 +18,12 @@ ALLEGRO_DISPLAY *janela = NULL;
 ALLEGRO_EVENT_QUEUE *event_queue = NULL;
 ALLEGRO_BITMAP *imagem = NULL;
 
-coordenadas posicoes[46];
+int main(){
 
-void AlgDijkstra( Digraph G, Vertex s, Vertex parent[],
-                   double dist[])
-{
-   Vertex v0, w, frj[maxV];
-   link2 a; double c;
-   initialize( G, s, parent, dist, frj);
+	Digraph teste = DIGRAPHinit(46);
+	double a[46];
 
-   while (1) {
-      double mindist = INFINITO;
-      for (w = 0; w < G->V; w++)
-         if (parent[w] == -1 && mindist > dist[w])
-            mindist = dist[v0=w];
-      if (mindist == INFINITO) break;
-      parent[v0] = frj[v0];
-      for (a = G->adj[v0]; a != NULL; a = a->next) {
-         w = a->w, c = a->cost;
-         if (parent[w] == -1 && dist[w] > dist[v0] + c) {
-            dist[w] = dist[v0] + c;
-            frj[w] = v0;
-         }
-      }
-   }
-}
-
-
-void routeConstruct(Vertex w, Vertex pi[]){
-   while(w != BASE){
-          //printf("%\nh%d\n", w);
-      al_draw_line(posicoes[w].x,posicoes[w].y, posicoes[pi[w]].x, posicoes[pi[w]].y, al_map_rgb(0,255,0),4);
-      w = pi[w];
-
-   }
-
-}
-
-void insereArestas(Digraph teste){
-DIGRAPHinsertA(teste, 1, 7, 120);
-DIGRAPHinsertA(teste, 1, 8, 120);
-
-DIGRAPHinsertA(teste, 2, 8, 120);
-DIGRAPHinsertA(teste, 2, 9, 120);
-
-DIGRAPHinsertA(teste, 3, 9, 120);
-DIGRAPHinsertA(teste, 3, 10, 130);
-
-DIGRAPHinsertA(teste, 4, 10, 130);
-
-DIGRAPHinsertA(teste, 5, 16, 240);
-DIGRAPHinsertA(teste, 5, 11, 130);
-
-DIGRAPHinsertA(teste, 6, 11, 130);
-DIGRAPHinsertA(teste, 6, 12, 120);
-
-DIGRAPHinsertA(teste, 7, 13, 110);
-
-DIGRAPHinsertA(teste, 8, 13, 110);
-DIGRAPHinsertA(teste, 8, 14, 130);
-
-DIGRAPHinsertA(teste, 9, 14, 120);
-DIGRAPHinsertA(teste, 9, 15, 140);
-
-DIGRAPHinsertA(teste, 10, 15, 120);
-DIGRAPHinsertA(teste, 10, 16, 150);
-
-DIGRAPHinsertA(teste, 11, 17, 120);
-
-DIGRAPHinsertA(teste, 12, 17, 120);
-
-DIGRAPHinsertA(teste, 13, 18, 120);
-DIGRAPHinsertA(teste, 13, 19, 140);
-
-DIGRAPHinsertA(teste, 14, 19, 120);
-DIGRAPHinsertA(teste, 14, 20, 140);
-
-DIGRAPHinsertA(teste, 15, 20, 120);
-DIGRAPHinsertA(teste, 15, 21, 140);
-
-DIGRAPHinsertA(teste, 16, 21, 110);
-DIGRAPHinsertA(teste, 16, 22, 120);
-
-DIGRAPHinsertA(teste, 17, 23, 120);
-DIGRAPHinsertA(teste, 17, 24, 120);
-
-DIGRAPHinsertA(teste, 18, 25, 130);
-
-DIGRAPHinsertA(teste, 19, 25, 120);
-DIGRAPHinsertA(teste, 19, 26, 75);
-DIGRAPHinsertA(teste, 19, 27, 140);
-
-DIGRAPHinsertA(teste, 20, 27, 110);
-DIGRAPHinsertA(teste, 20, 28, 140);
-
-DIGRAPHinsertA(teste, 21, 28, 110);
-DIGRAPHinsertA(teste, 21, 29, 120);
-
-DIGRAPHinsertA(teste, 22, 29, 120);
-DIGRAPHinsertA(teste, 22, 30, 110);
-
-DIGRAPHinsertA(teste, 23, 30, 120);
-DIGRAPHinsertA(teste, 23, 31, 110);
-
-DIGRAPHinsertA(teste, 24, 31, 120);
-
-DIGRAPHinsertA(teste, 25, 32, 120);
-DIGRAPHinsertA(teste, 25, 33, 75);
-DIGRAPHinsertA(teste, 25, 34, 140);
-
-DIGRAPHinsertA(teste, 26, 33, 110);
-DIGRAPHinsertA(teste, 26, 27, 57);
-
-DIGRAPHinsertA(teste, 27, 34, 120);
-DIGRAPHinsertA(teste, 27, 35, 150);
-
-DIGRAPHinsertA(teste, 28, 35, 130);
-DIGRAPHinsertA(teste, 28, 36, 120);
-
-DIGRAPHinsertA(teste, 29, 36, 120);
-DIGRAPHinsertA(teste, 29, 37, 120);
-
-DIGRAPHinsertA(teste, 30, 38, 130);
-DIGRAPHinsertA(teste, 30, 37, 120);
-
-DIGRAPHinsertA(teste, 31, 38, 120);
-DIGRAPHinsertA(teste, 31, 39, 120);
-
-DIGRAPHinsertA(teste, 32, 40, 140);
-
-DIGRAPHinsertA(teste, 33, 34, 63);
-
-DIGRAPHinsertA(teste, 34, 40, 120);
-DIGRAPHinsertA(teste, 34, 41, 140);
-
-DIGRAPHinsertA(teste, 35, 41, 130);
-DIGRAPHinsertA(teste, 35, 42, 120);
-
-DIGRAPHinsertA(teste, 36, 42, 100);
-DIGRAPHinsertA(teste, 36, 43, 130);
-
-DIGRAPHinsertA(teste, 37, 43, 130);
-DIGRAPHinsertA(teste, 37, 44, 110);
-
-DIGRAPHinsertA(teste, 38, 44, 120);
-DIGRAPHinsertA(teste, 38, 45, 120);
-
-DIGRAPHinsertA(teste, 39, 45, 110);
-
-}
-
-int main()
-{
-
-    Digraph teste = DIGRAPHinit(46);
-
-
-double a[46];
-
-insereArestas(teste);
+	insereArestas(teste);
 
 //coordenadas posicoes[46];
 posicoes[1].x = 70;
@@ -288,31 +135,23 @@ for(i = 1; i < 46; i++){
 printf("\n");
 
 
-    if (!inicializar())
-    {
+    if (!inicializar()){
         return -1;
     }
-al_clear_to_color(al_map_rgb(0, 0, 0));
 
-   al_flip_display();
-       while(1)
-   {
+    al_clear_to_color(al_map_rgb(0, 0, 0));
+	al_flip_display();
+	
+    while(1){
+		al_draw_bitmap(imagem, 0,0,0);
+      	ALLEGRO_EVENT ev;
+      	al_wait_for_event(event_queue, &ev);
 
-al_draw_bitmap(imagem, 0,0,0);
-      ALLEGRO_EVENT ev;
-      al_wait_for_event(event_queue, &ev);
+      	if(ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
+       	  break;
+      	}
 
-      if(ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
-         break;
-      }
-
-
-
-
-      desenhar(ev, ev.mouse.x, ev.mouse.y);
-
-
-
+		desenhar(ev, ev.mouse.x, ev.mouse.y);
    }
 
    al_destroy_display(janela);
--- graph.h
+++ graph.h
@@ -1,3 +1,6 @@
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_primitives.h>
+
 #define Vertex int
 #define INFINITO 100000
 #define maxV 100
@@ -24,6 +27,8 @@ struct coord
    int y;
 };
 typedef struct coord coordenadas;
+
+coordenadas posicoes[46];
 
 link2 NEWnode( Vertex w, double cost, link2 next) {
    link2 a = malloc( sizeof (struct node));
@@ -83,4 +88,152 @@ void initialize( Digraph G, Vertex s, Vertex parent[], double dist[], Vertex frj
       dist[w] = c;
       frj[w] = s;
    }
+}
+
+void AlgDijkstra( Digraph G, Vertex s, Vertex parent[],
+                   double dist[])
+{
+   Vertex v0, w, frj[maxV];
+   link2 a; double c;
+   initialize( G, s, parent, dist, frj);
+
+   while (1) {
+      double mindist = INFINITO;
+      for (w = 0; w < G->V; w++)
+         if (parent[w] == -1 && mindist > dist[w])
+            mindist = dist[v0=w];
+      if (mindist == INFINITO) break;
+      parent[v0] = frj[v0];
+      for (a = G->adj[v0]; a != NULL; a = a->next) {
+         w = a->w, c = a->cost;
+         if (parent[w] == -1 && dist[w] > dist[v0] + c) {
+            dist[w] = dist[v0] + c;
+            frj[w] = v0;
+         }
+      }
+   }
+}
+
+
+void routeConstruct(Vertex w, Vertex pi[]){
+   while(w != BASE){
+          //printf("%\nh%d\n", w);
+      al_draw_line(posicoes[w].x,posicoes[w].y, posicoes[pi[w]].x, posicoes[pi[w]].y, al_map_rgb(0,255,0),4);
+      w = pi[w];
+
+   }
+
+}
+
+void insereArestas(Digraph teste){
+   DIGRAPHinsertA(teste, 1, 7, 120);
+   DIGRAPHinsertA(teste, 1, 8, 120);
+   
+   DIGRAPHinsertA(teste, 2, 8, 120);
+   DIGRAPHinsertA(teste, 2, 9, 120);
+   
+   DIGRAPHinsertA(teste, 3, 9, 120);
+   DIGRAPHinsertA(teste, 3, 10, 130);
+   
+   DIGRAPHinsertA(teste, 4, 10, 130);
+   
+   DIGRAPHinsertA(teste, 5, 16, 240);
+   DIGRAPHinsertA(teste, 5, 11, 130);
+   
+   DIGRAPHinsertA(teste, 6, 11, 130);
+   DIGRAPHinsertA(teste, 6, 12, 120);
+   
+   DIGRAPHinsertA(teste, 7, 13, 110);
+   
+   DIGRAPHinsertA(teste, 8, 13, 110);
+   DIGRAPHinsertA(teste, 8, 14, 130);
+   
+   DIGRAPHinsertA(teste, 9, 14, 120);
+   DIGRAPHinsertA(teste, 9, 15, 140);
+   
+   DIGRAPHinsertA(teste, 10, 15, 120);
+   DIGRAPHinsertA(teste, 10, 16, 150);
+   
+   DIGRAPHinsertA(teste, 11, 17, 120);
+   
+   DIGRAPHinsertA(teste, 12, 17, 120);
+   
+   DIGRAPHinsertA(teste, 13, 18, 120);
+   DIGRAPHinsertA(teste, 13, 19, 140);
+   
+   DIGRAPHinsertA(teste, 14, 19, 120);
+   DIGRAPHinsertA(teste, 14, 20, 140);
+   
+   DIGRAPHinsertA(teste, 15, 20, 120);
+   DIGRAPHinsertA(teste, 15, 21, 140);
+   
+   DIGRAPHinsertA(teste, 16, 21, 110);
+   DIGRAPHinsertA(teste, 16, 22, 120);
+   
+   DIGRAPHinsertA(teste, 17, 23, 120);
+   DIGRAPHinsertA(teste, 17, 24, 120);
+   
+   DIGRAPHinsertA(teste, 18, 25, 130);
+   
+   DIGRAPHinsertA(teste, 19, 25, 120);
+   DIGRAPHinsertA(teste, 19, 26, 75);
+   DIGRAPHinsertA(teste, 19, 27, 140);
+   
+   DIGRAPHinsertA(teste, 20, 27, 110);
+   DIGRAPHinsertA(teste, 20, 28, 140);
+   
+   DIGRAPHinsertA(teste, 21, 28, 110);
+   DIGRAPHinsertA(teste, 21, 29, 120);
+   
+   DIGRAPHinsertA(teste, 22, 29, 120);
+   DIGRAPHinsertA(teste, 22, 30, 110);
+   
+   DIGRAPHinsertA(teste, 23, 30, 120);
+   DIGRAPHinsertA(teste, 23, 31, 110);
+   
+   DIGRAPHinsertA(teste, 24, 31, 120);
+   
+   DIGRAPHinsertA(teste, 25, 32, 120);
+   DIGRAPHinsertA(teste, 25, 33, 75);
+   DIGRAPHinsertA(teste, 25, 34, 140);
+   
+   DIGRAPHinsertA(teste, 26, 33, 110);
+   DIGRAPHinsertA(teste, 26, 27, 57);
+   
+   DIGRAPHinsertA(teste, 27, 34, 120);
+   DIGRAPHinsertA(teste, 27, 35, 150);
+   
+   DIGRAPHinsertA(teste, 28, 35, 130);
+   DIGRAPHinsertA(teste, 28, 36, 120);
+   
+   DIGRAPHinsertA(teste, 29, 36, 120);
+   DIGRAPHinsertA(teste, 29, 37, 120);
+   
+   DIGRAPHinsertA(teste, 30, 38, 130);
+   DIGRAPHinsertA(teste, 30, 37, 120);
+   
+   DIGRAPHinsertA(teste, 31, 38, 120);
+   DIGRAPHinsertA(teste, 31, 39, 120);
+   
+   DIGRAPHinsertA(teste, 32, 40, 140);
+   
+   DIGRAPHinsertA(teste, 33, 34, 63);
+   
+   DIGRAPHinsertA(teste, 34, 40, 120);
+   DIGRAPHinsertA(teste, 34, 41, 140);
+   
+   DIGRAPHinsertA(teste, 35, 41, 130);
+   DIGRAPHinsertA(teste, 35, 42, 120);
+   
+   DIGRAPHinsertA(teste, 36, 42, 100);
+   DIGRAPHinsertA(teste, 36, 43, 130);
+   
+   DIGRAPHinsertA(teste, 37, 43, 130);
+   DIGRAPHinsertA(teste, 37, 44, 110);
+   
+   DIGRAPHinsertA(teste, 38, 44, 120);
+   DIGRAPHinsertA(teste, 38, 45, 120);
+   
+   DIGRAPHinsertA(teste, 39, 45, 110);
+
 }
